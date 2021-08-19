07443035a93b84de4f732aa9090b3b765fa4f0bd
mahonec <mahonec@clarkson.edu>
2021-08-18 22:56:52 -0400

    delete default code and started top navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <navbar />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import Navbar from "./components/navigation/Navbar.vue";
+
+export default defineComponent({
+  components: {
+    Navbar,
+  },
+});
+</script>
+
 <style lang="scss">
+html body {
+  overflow-x: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  width: 100vw;
+  height: 100vh;
 }
 </style>
---
+++ src/components/navigation/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul id="nav">
+    <li v-for="link of links" :key="link">
+      <span> {{ link }} </span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => {
+    return {
+      links: ["Hello", "World", "Photos"],
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  width: 100%;
+  height: 80px;
+  margin: 0px;
+  position: absolute;
+  top: 0px;
+
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+
+  background: rgb(36, 36, 36);
+  color: white;
+  font-optical-sizing: auto;
+  font-size: 22px;
+
+  li {
+    list-style-type: none;
+  }
+}
+</style>
